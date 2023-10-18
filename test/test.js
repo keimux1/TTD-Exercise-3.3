c8415ca6b458b642358116c826ee5e46f8f8d47b
Emulu Keith <keimux21@gmail.com>
2023-10-19 02:22:47 +0300

    the fuction is working as expected

--- test/test.js
+++ test/test.js
@@ -3,9 +3,12 @@ const taxCalculation = require("../js/taxCalculation");
 
 describe("testing taxCalculation function", function (){
 
-    it("n<1200 = 0",function(){
+    it("n<12000 = 0",function(){
         assert.equal(taxCalculation(12000), 0);
     });
 
+    it("12000<n<=36000",function(){
+        assert.equal(taxCalculation(20000),16000);
+    });
 
 });
