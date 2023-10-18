74170ca60997ef374eeb7a4ebd8f3a05376d9365
Emulu Keith <keimux21@gmail.com>
2023-10-19 02:17:51 +0300

    the fuction was modifiesd and seems to perform as expected

--- js/taxCalculation.js
+++ js/taxCalculation.js
@@ -1,8 +1,8 @@
 let taxableIncome = (n) => {
     let tax;
-    if (n < 12000) {
+    if (n <= 12000) {
         tax = 0;
-    } else if (12000 < n && n < 36000) {
+    } else if (12000 < n && n <= 36000) {
         tax = n - (n * 0.2);
     } else {
         tax = (n - (n * 0.4)) + (n - (n * 0.2));
